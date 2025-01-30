164cd96252053a021a4c9488c8267a6dcc2fdaba
Collin Bowdoin <cjbowdoin@gmail.com>
2025-01-29 22:39:36 -0800

    Initial Commit

---
+++ MHDungeon/MHDungeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHDungeonApp.swift
+//  MHDungeon
+//
+//  Created by Collin Bowdoin on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MHDungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MHDungeonTests/MHDungeonTests.swift
@@ -0,0 +1,17 @@
+//
+//  MHDungeonTests.swift
+//  MHDungeonTests
+//
+//  Created by Collin Bowdoin on 1/29/25.
+//
+
+import Testing
+@testable import MHDungeon
+
+struct MHDungeonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
