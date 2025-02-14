3266fd1722d449356e59187971b29c020a39b46b
Collin Bowdoin <cjbowdoin@gmail.com>
2025-02-13 17:00:55 -0800

    Implemented animations on navigation bar. Included reset on Task button, but not the rest - WIP

---
+++ MHDungeon/Views/Profile/ProgressDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressDetails.swift
+//  MHDungeon
+//
+//  Created by Collin Bowdoin on 2/13/25.
+//
+
+import SwiftUI
+
+struct ProgressDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressDetails()
+}
